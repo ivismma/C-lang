77ed77e39e58f92e38ed9cbd49ac07461721f5cc
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-02-21 06:15:03 -0300

    Add gabaritos do meu curso
    Gabaritos do meu curso de C.

--- Desafio obterDigito().c
+++ Desafio obterDigito().c
@@ -18,41 +18,41 @@ int main(void){
 }
 
 int obterDigito(int n, char letra){
-	int i, digito;
+	int i;
 	n = (n < 0)? -n : n;    // Evitar retorno negativo.
 	
 	if(letra == 'u') 
-		return n%10;        // A unidade do valor inserido é o que procuramos.
+		return n%10;        // A unidade do valor inserido Ã© o que procuramos.
 	else if(letra == 'd') 
-		return n/10%10;     // Necessário pular um dígito
+		return n/10%10;     // NecessÃ¡rio pular um dÃ­gito
 	else if(letra == 'c') 
-		i = n/100%10;       // Necessário pular dois dígitos
+		i = n/100%10;       // NecessÃ¡rio pular dois dÃ­gitos
 	else if(letra == 'm') 
-		i = n/1000%10;      // Necessário pular três dígitos
+		i = n/1000%10;      // NecessÃ¡rio pular trÃªs dÃ­gitos
 	else 
-		return -1;          // Letra inválida.
+		return -1;          // Letra invÃ¡lida.
 	
 }
 
 /*
 
-9. DESAFIO: Na Lista 2 - Questão 3, Tivemos um exercício o qual, a partir de
-um inteiro com 5 dígitos, deveria ser impresso a unidade desse valor.
-Dessa vez, crie uma função que receba um inteiro e uma letra, o valor
-inserido deverá possuir 4 dígitos. Veja o que o programa deverá retornar
+9. DESAFIO: Na Lista 2 - QuestÃ£o 3, Tivemos um exercÃ­cio o qual, a partir de
+um inteiro com 5 dÃ­gitos, deveria ser impresso a unidade desse valor.
+Dessa vez, crie uma funÃ§Ã£o que receba um inteiro e uma letra, o valor
+inserido deverÃ¡ possuir 4 dÃ­gitos. Veja o que o programa deverÃ¡ retornar
 em cada caso:
-Sugestão de assinatura da função: int obterDigito(int n, char letra);
+SugestÃ£o de assinatura da funÃ§Ã£o: int obterDigito(int n, char letra);
 Letra inserida - Retornar algarismo
-‘u’: - Retornar a unidade do valor.
-‘d’ - Retornar a dezena do valor.
-‘c’ - Retornar a centena do valor.
-‘m’ - Retornar a unidade de milhar do valor.
-Se outra letra for inserida, o programa deverá retornar -1.
-São exemplos de entrada e saída da função:
-ENTRADA    SAÍDA
-1032, ‘d’    3
-2024, ‘u’    4
-2024, ‘c’    0
-1723, ‘x’   -1
+â€˜uâ€™: - Retornar a unidade do valor.
+â€˜dâ€™ - Retornar a dezena do valor.
+â€˜câ€™ - Retornar a centena do valor.
+â€˜mâ€™ - Retornar a unidade de milhar do valor.
+Se outra letra for inserida, o programa deverÃ¡ retornar -1.
+SÃ£o exemplos de entrada e saÃ­da da funÃ§Ã£o:
+ENTRADA    SAÃDA
+1032, â€˜dâ€™    3
+2024, â€˜uâ€™    4
+2024, â€˜câ€™    0
+1723, â€˜xâ€™   -1
 
 */
