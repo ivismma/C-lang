2521f5dbe9a8d6b51c4f1e121ef5ac922a2ecbc1
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2024-04-16 22:54:55 -0300

    intercalação recursiva (sem i e j)
    intercalação recursiva sem parâmetros i e j. redução de parâmetros na func, menor consumo de memória

--- intercalação recursiva.c
+++ intercalação recursiva.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#define TAM_V 4
+#define TAM_V 5
 
-// IntercalaÁ„o de arrays --> recursiva.
-// FunÁ„o contempl·vel por tail call optimization! -O2 no gcc
+// Intercala√ß√£o de arrays --> recursiva.
+// Fun√ß√£o contempl√°vel por tail call optimization! -O2 no gcc
 
 void intercalaRec(int *v1, int *v2);
-void recursao(int *v1, int *v2, int i, int j, int cont);
+void recursao(int *v1, int *v2, int cont); // sem usar i e j.
 
 int main(void){
-	int v1[TAM_V] = {1,3,5,7};
-	int v2[TAM_V] = {2,4,6,8};
+	int v1[TAM_V] = {1,3,5,7,9};
+	int v2[TAM_V] = {2,4,6,8,10};
 	
 	intercalaRec(v1, v2);
 	
@@ -19,10 +19,31 @@ int main(void){
 
 // Tornar a chamada da func. menos verbosa (=
 void intercalaRec(int *v1, int *v2){
-	recursao(v1, v2, 0, 0, 0);
+	recursao(v1, v2, 0);
+}
+
+void recursao(int *v1, int *v2, int cont){
+	// Caso Base
+	if(cont == 2*TAM_V)
+		return;
+	
+	// Caso geral...
+	if(cont%2 == 0){
+		printf("%d ", v1[cont/2]);
+		recursao(v1,v2,cont+1);
+	}
+	else{
+		printf("%d ", v2[cont/2]);
+		recursao(v1,v2,cont+1);
+	}	
 }
 
 
+
+
+
+// Obsoletas (menos otimizadas)
+/*
 void recursao(int *v1, int *v2, int i, int j, int cont){
 	// Caso Base
 	if(cont == 2*TAM_V)
@@ -38,3 +59,20 @@ void recursao(int *v1, int *v2, int i, int j, int cont){
 		recursao(v1,v2, i, j+1, cont+1);
 	}	
 }
+
+void recursao2(int *v1, int *v2, int i, int cont){
+	// Caso Base
+	if(cont == 2*TAM_V)
+		return;
+	
+	// Caso geral...
+	if(cont%2 == 0){
+		printf("%d ", v1[i]);
+		recursao2(v1,v2, i, cont+1);
+	}
+	else{
+		printf("%d ", v2[i]);
+		recursao2(v1,v2, i+1,  cont+1);
+	}	
+}
+*/
