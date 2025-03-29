a73938c46c1837a7d1c5c5455bc0015fa7c82197
Ivis Muzi <118779139+ivismma@users.noreply.github.com>
2025-03-29 00:48:29 -0300

    Min Stack

---
+++ Leetcode #155.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int KEYTYPE;
+
+// nó da pilha
+typedef struct _StackNode{
+	KEYTYPE key;
+	int min; // diz quem é o minimo da pilha até ESSE nó.
+	struct _StackNode *next;
+} StackNode;
+
+// estrutura da pilha
+typedef struct _MinStack{
+    StackNode *head;
+} MinStack;
+
+MinStack* minStackCreate();
+void minStackPush(MinStack* obj, KEYTYPE val);
+void minStackPop(MinStack* obj);
+int minStackTop(MinStack* obj);
+int minStackGetMin(MinStack* obj);
+void minStackFree(MinStack* obj);
+
+int main(void){
+	// MinStack *stack = minStackCreate();
+	
+	// minStackFree(stack);
+	return 0;
+}
+
+
+MinStack* minStackCreate() {
+    MinStack *stack = (MinStack *) malloc(sizeof(MinStack));
+    if(stack == NULL) // falha aloc
+    	exit(1);
+    
+    stack->head = NULL;
+    
+    return stack;
+}
+
+void minStackPush(MinStack* obj, int val) {
+	StackNode *newNode = (StackNode *) malloc(sizeof(StackNode));
+	newNode->key = val;
+	
+	if(newNode == NULL) 
+		exit(1);
+	
+	if(obj->head == NULL){
+		obj->head = newNode;
+		newNode->next = NULL;
+		newNode->min = val;
+	}
+	else{
+		newNode->next = obj->head;
+		if(val < obj->head->min)
+			newNode->min = val;
+		else
+			newNode->min = obj->head->min;
+		
+		obj->head = newNode;
+	}
+}
+
+void minStackPop(MinStack* obj) {
+    StackNode *remove = obj->head;
+	if(remove == NULL)
+    	exit(2); // underflow
+    
+    obj->head = remove->next;
+    free(remove);
+}
+
+int minStackTop(MinStack* obj) {
+    if(obj->head == NULL)
+    	exit(2);
+    
+    return obj->head->key;
+}
+
+int minStackGetMin(MinStack* obj) {
+    if(obj->head == NULL)
+    	exit(2);
+    
+    return obj->head->min;
+}
+
+void minStackFree(MinStack* obj) {
+	if(obj == NULL || obj->head == NULL)
+		return;
+	
+    StackNode *current = obj->head;
+    while(current != NULL){
+		StackNode *remove = current;
+		current = current->next;
+		free(remove);
+	}
+}
